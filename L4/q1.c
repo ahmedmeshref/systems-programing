8aa043a091776ff7b12108060d9a7051f1c2b27e
ahmedmeshref <a.meshref@alustudent.com>
2020-10-19 00:36:45 +0200

    Add sol for q1

---
+++ L4/q1.c
@@ -0,0 +1,35 @@
+// Write a C program to create a file called EmplRecord.txt and store information about a person,
+//  in terms of his/her name, age, and salary.
+#include <stdio.h>
+
+int main()
+
+{
+    // Declare the file pointer in the write mode
+    FILE *ptr = fopen("EmplRecord.txt", "w");
+
+    // Declar user info (name, age, salary)
+    char *name = "Mohamed";
+    char* age = "20";
+    char* salary = "1000.0";
+
+    // check if file exist
+    if (ptr == NULL)
+    {
+        printf("file failed to open.");
+        return 1;
+    }
+    // write data to the file 
+    fputs(name, ptr);
+    fputs("\n", ptr);
+    fputs(age, ptr);
+    fputs("\n", ptr);
+    fputs(salary, ptr);
+    fputs("\n", ptr);
+
+    // Closing the file
+    fclose(ptr);
+    printf("Data successfully written in file EmplRecord.txt\n");
+    printf("The file is now closed.\n");
+    return 0;
+}   
