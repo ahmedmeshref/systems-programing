9dcbab32f0b0aef47bedd0d3f3df901fe532cd6d
ahmedmeshref <a.meshref@alustudent.com>
2020-10-21 12:18:24 +0200

    Add solution for q4

--- L4/q4.c
+++ L4/q4.c
@@ -1,47 +1,75 @@
 // Write a program in C to replace a specific line with another text in a file
-// Input the file name to be opened : test.txt
-
-// Input the content of the new line : Yes, I am the new text instead of test line 2
-
-// Input the line no you want to replace : 2
 
 #include <stdio.h> 
 #include <stdbool.h>
 #include <stdlib.h>
 
 int get_positive_number(void);
-void replace_line (char* fName, char* newLine, int lineNum);
+int replace_line (char fName[], char newLine[], int lineNum);
 
 int main(void)
 {   
+     // get new line text
+    char text[255];
+    printf("Input the content of the new line: ");
+    fgets(text, sizeof(text), stdin);
+
     // get fileName 
     char fName[30];
     printf("Input the file name to be opened: ");
-    fgets(fName, sizeof(fName), stdin);
-
-    // get new line text
-    char text[500];
-    printf("Input the content of the new line: ");
-    fgets(text, sizeof(text), stdin);
+    scanf("%s", fName);
 
     // get line number
     int lineNum = get_positive_number();
 
-    replace_line(fName, text, lineNum);
-    return 0;
+    // repalce line and return 0 if successful, 1 otherwise.
+    if (replace_line(fName, text, lineNum) == 1) return 1;
+    else return 0;
+    
 }
 
-void replace_line (char* fName, char* newLine, int lineNum)
+int replace_line (char fName[], char newLine[], int lineNum)
 {
+    // open the main file in read mode
     FILE* ptr = fopen(fName, "r");
     if (ptr == NULL)
     {
-        printf("file failed to open.");
+        printf("Input file failed to open.\n");
         return 1;
     }
-    // open a temp file and write all lines except the lineNum -> write newLine 
-    // remove(ptr);
-    // replace the name of the temp to the fName
+    // create a new file with temp name to save the new data
+    char tempName[] = "file.txt";
+    FILE* ptr_2 = fopen(tempName, "w");
+    if (ptr_2 == NULL)
+    {
+        printf("Output file failed to open.\n");
+        return 1;
+    }
+    
+    // create a buffer to hold each line's data
+    int bufferLength = 255;
+    char buffer[bufferLength];
+    int currentLineNum = 1;
+    while (fgets(buffer, bufferLength, ptr)) {
+        if (currentLineNum == lineNum) fputs(newLine, ptr_2);
+        else fputs(buffer, ptr_2);
+        currentLineNum++;
+    }
+    // if the line was not added, add it on a new line at the end of the temp file. 
+    if (currentLineNum < lineNum) {
+        fputc('\n', ptr_2);
+        fputs(newLine, ptr_2);
+    }
+
+    // close all files
+    fclose(ptr);
+    fclose(ptr_2);
+    // remove the old file
+    remove(fName);
+    // rename the temp file to have the name of the deleted file
+    rename(tempName, fName);
+    printf("Replacement did successfully..!!\n");
+    return 0;
 }
 
 
