802b7c7d8d6f5b764973528c980d4400cf5476e0
ahmedmeshref <a.meshref@alustudent.com>
2020-11-23 14:08:21 +0200

    Finalize ex1

--- S11_shell/sys_var.sh
+++ S11_shell/sys_var.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
+# the shell used by the user
 curr_shell=$SHELL
-printf "shell: ${curr_shell}\n"
+printf "Shell: ${curr_shell}\n"
 
+# the user name
 user=$(whoami)
-printf "username: ${user}\n"
+printf "Username: ${user}\n"
 
-user_id=$(id)
-# split the id from the string 
-printf "${user_id}"
+# user id
+echo "UserId: $UID"
+# OR
+id=$(id)
+echo ${id} | cut -c1-8
+
+# home directory
+echo "Home: $HOME"
+
+# calender
+calender=`cal`
+echo -e "---- My Calender ---- \n ${calender}\n"
+
